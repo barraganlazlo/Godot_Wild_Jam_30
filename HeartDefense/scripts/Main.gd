76c1116c55ed6117c782dd720bbce324a9c2b087
lazlo barragan <lazlo.barragan@gmail.com>
2021-02-15 15:39:25 +0100

    name changes

--- HeartDefense/scripts/Main.gd
+++ HeartDefense/scripts/Main.gd
@@ -2,8 +2,9 @@ extends Node
 onready var room_tile_size: Vector2 = Vector2(40.0,27.5)
 onready var room_pixel_size: Vector2 = room_tile_size * 16.0
 
-onready var heart_building:= $YSort/Building
+onready var heart_building:= $YSort/HeartBuilding
 onready var player:= $YSort/Player
+onready var buildings:= $YSort/Buildings
 
 func _ready() -> void: 
 	heart_building.connect("heart_beat", self, "heart_beat")
